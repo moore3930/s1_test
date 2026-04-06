d0a080953fee28e25e690e09d582f0571d57c0d7
research-anonymous <moore.wods@gmail.com>
2026-04-06 20:57:08 +0200

    update

--- eval/lm-evaluation-harness/commands_sequential_2.sh
+++ eval/lm-evaluation-harness/commands_sequential_2.sh
@@ -20,4 +20,4 @@ export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 NIgnore=$1
 OPENAI_KEY=xx
 
-OPENAI_API_KEY=YOUR_OPENAI_KEY PROCESSOR=gpt-4o-mini lm_eval --model vllm --model_args pretrained=simplescaling/s1-32B,tokenizer=Qwen/Qwen2.5-32B-Instruct,dtype=float32,tensor_parallel_size=8 --tasks aime24_figures,aime24_nofigures,openai_math,gpqa_diamond_openai --batch_size auto --apply_chat_template --output_path forcingignore1wait --log_samples --gen_kwargs "max_gen_toks=32768,max_tokens_thinking=auto,thinking_n_ignore=$NIgnore,thinking_n_ignore_str=Wait"
+OPENAI_API_KEY=YOUR_OPENAI_KEY PROCESSOR=gpt-4o-mini lm_eval --model vllm --model_args pretrained=simplescaling/s1-32B,tokenizer=Qwen/Qwen2.5-32B-Instruct,dtype=float32,tensor_parallel_size=8 --tasks aime24_figures,aime24_nofigures,openai_math,gpqa_diamond_openai --batch_size auto --apply_chat_template --output_path forcing/forcingignore${NIgnore}wait --log_samples --gen_kwargs "max_gen_toks=32768,max_tokens_thinking=auto,thinking_n_ignore=$NIgnore,thinking_n_ignore_str=Wait"
